ad1b2da77216dd1545156cf111314d30f05e2864
rominpatel <patelromin05@gmail.com>
2022-04-24 22:48:47 +0100

    first commit

---
+++ MultiplicationTables.java
@@ -0,0 +1,11 @@
+package For;
+public class MultiplicationTables {
+    public static void main(String[] args) {
+        int number = 10;
+        for (int i = 0; i <=10; i++){
+        System.out.println(number + "*" + i + "=" + (number * i));
+        }
+    }
+
+    }
+
