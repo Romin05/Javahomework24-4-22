39187d2b07997f4eba199e818d3a53b3de83b2f7
rominpatel <patelromin05@gmail.com>
2022-05-02 09:52:17 +0100

    Javahomeworkpattern

--- ForLoop.java
+++ ForLoop.java
@@ -1,14 +1,56 @@
 package For;
 
 public class ForLoop {
+    //public static void main(String[] args) {
+    //}
+    //     {
+    //       for (int i = 1; i <=10; i = i + 2) {
+    //             System.out.println(i);
+    //         }
+    //         for (int i = 2; i <=10; i = i + 2) {
+    //             System.out.println(i);
+    //         }
+    //     }
+    //  }
+
+
+    //   public static void main(String[] args) {
+//
+//      for (int a = 0; a <=0; a++){
+    //        System.out.println("*");
+    //      System.out.println("**");
+    //    System.out.println("***");
+    //  System.out.println("****");
+    //System.out.println("*****");
+    // }
+    //}
+
     public static void main(String[] args) {
-        {
-            for (int i = 1; i <=10; i = i + 2) {
-                System.out.println(i);
-            }
-            for (int i = 2; i <=10; i = i + 2) {
-                System.out.println(i);
-            }
+
+        for (int a = 0; a <= 0; a++) {
+            System.out.println("*");
+            System.out.println("* *");
+            System.out.println("* * *");
+            System.out.println("* * * *");
+            System.out.println("* * * * *");
+
+        }
+        for (int a = 0; a <= 0; a++) {
+            System.out.println("    *   ");
+            System.out.println("   * *  ");
+            System.out.println("  * * * ");
+            System.out.println(" * * * *");
+            System.out.println("* * * * *");
+
+        }
+        for (int a = 0; a <= 0; a++) {
+            System.out.println("         *");
+            System.out.println("       * *");
+            System.out.println("     * * *");
+            System.out.println("   * * * *");
+            System.out.println(" * * * * *");
+
+
         }
     }
 }
